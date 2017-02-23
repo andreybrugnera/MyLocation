a30dece3b8bee0cd4a9ea2504f6198cdbbc11d90
Bruno <b_britoabreu@hotmail.com>
2017-02-22 23:18:40 -0300

    Merge branch 'master' of https://github.com/andreybrugnera/MyLocation
    # Conflicts:
    #	app/src/main/java/br/edu/ifsp/mylocation/MainActivity.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/br/edu/ifsp/mylocation/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/mylocation/MainActivity.java
@@ -1,11 +1,14 @@
 package br.edu.ifsp.mylocation;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -87,11 +90,32 @@ public class MainActivity extends Activity implements
         stopLocationUpdates();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) ||
+                    permissions[0].equals(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED || grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permissão aceita", Toast.LENGTH_SHORT).show();
+                    loadLastLocation();
+                } else {
+                    Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
     /**
      * Carrega a última localização
      * do dispositivo
      */
     public void loadLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
         try{
             lastLocation = LocationServices.FusedLocationApi.getLastLocation(
                     googleApiClient);
@@ -162,6 +186,11 @@ public class MainActivity extends Activity implements
      * Inicia a atualização da localização de tempos em tempos
      */
     protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
         try {
             LocationServices.FusedLocationApi.requestLocationUpdates(
                     googleApiClient, locationRequest, this);
